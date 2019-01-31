312073f874841707b54251a28dd88fe6e45656c0
lilin <lilin8921@adpanshi.com>
2019-01-31 14:09:08 +0800

    引进数据库和Redis配置

---
+++ src/main/java/com/example/port/common/dao/TkMapper.java
@@ -0,0 +1,11 @@
+package com.example.port.common.dao;
+
+import tk.mybatis.mapper.common.MySqlMapper;
+
+/**
+ * @author lilin
+ * @date 2019-01-31
+ * @param <T>
+ */
+public interface TkMapper<T> extends MySqlMapper<T> {
+}
