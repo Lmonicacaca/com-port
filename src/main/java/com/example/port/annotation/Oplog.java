643c45caf85a672a6a8587b0d05927b5d60c7cab
lilin <396435308@qq.com>
2019-07-28 17:07:36 +0800

    添加Springcloud配置

---
+++ src/main/java/com/example/port/annotation/Oplog.java
@@ -0,0 +1,9 @@
+package com.example.port.annotation;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
+public @interface Oplog {
+}
--- src/main/java/com/example/port/controller/TestController.java
+++ src/main/java/com/example/port/controller/TestController.java
@@ -7,10 +7,7 @@ import com.example.port.service.TestService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author lilin
@@ -30,5 +27,10 @@ public class TestController {
         ResponseModel responseModel = new ResponseModel();
         testService.testJvm();
         return responseModel;
+    }
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "port:9999 hello";
     }
 }
